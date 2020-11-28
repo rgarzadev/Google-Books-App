83057149beb714c99232f1a142bfca42a86adcc3
Ryan Garza <ryangarza@live.com>
2020-11-27 22:16:56 -0600

    server update

--- server.js
+++ server.js
@@ -15,18 +15,8 @@ app.use([
     express.json()
 ]);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build")); 
-// }
-
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build")); 
 }
 
 //connect to local mongoDB googlebooks database using mongoose
